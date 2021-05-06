80ba89f9fcd4f79f6a893ed3ca222d43dd42c06b
Laura Moon <laura.kolb@gmail.com>
2021-05-06 13:48:40 -0500

    add genre search

--- MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/Controllers/MoviesController.cs
@@ -15,19 +15,34 @@ namespace MvcMovie.Controllers
         private MovieDBContext db = new MovieDBContext();
 
         // GET: Movies
-        public ActionResult Index(string searchString)
+        public ActionResult Index(string movieGenre, string searchString)
         {
             // to take advantage of the route configuration, could have the signature of this method as:
             // string id
             // and add here:
             // string searchString = id;
-            // then movies/index/[searchterm] would also perform a search
+            // then movies/index/[searchterm] would also perform a search for titles containing the string
+
+            var GenreLst = new List<string>();
+
+            var GenreQry = from d in db.Movies
+                           orderby d.Genre
+                           select d.Genre;
+
+            GenreLst.AddRange(GenreQry.Distinct());
+            ViewBag.movieGenre = new SelectList(GenreLst);
+
             var movies = from m in db.Movies
                          select m;
 
             if (!String.IsNullOrEmpty(searchString))
             {
                 movies = movies.Where(s => s.Title.Contains(searchString));
+            }
+
+            if (!string.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
             }
 
             return View(movies);
